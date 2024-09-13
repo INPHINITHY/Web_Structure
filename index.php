66bac169a2c2b71538b163e55dcd7352f50f57e2
Austin <dwomohaustin14@gmail.com>
2024-09-13 16:42:36 -0500

    frame

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Update the page title</title>
+<link href="ResponsiveTopNavDropdown.css" rel="stylesheet" type="text/css" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="doubletaptogo.min.js" type="text/javascript"></script>
+<!--[if IE]>
+	<script src="picturefill.min.js" async="async"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+	<script src="dist/html5shiv.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <div class="container">
+        <header class="header">
+        	<?php include('includes/nav.html'); ?>
+        </header>
+        
+        <main>
+            
+        </main>
+        
+        <footer class="footer">
+            <?php include('includes/footer.html'); ?>
+        </footer>
+    </div><!--end container-->
+</body>
+</html>
